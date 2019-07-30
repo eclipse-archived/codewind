ccc5e6b7a100cf6aed522f9e97c3a1767f015755
Toby Corbin <corbint@uk.ibm.com>
2019-07-30 11:32:59 +0100

    add tests
    Signed-off-by: Toby Corbin <corbint@uk.ibm.com>

--- test.sh
+++ test.sh
@@ -28,16 +28,8 @@ printf "\n\n${MAGENTA}Platform: $ARCH ${RESET}\n"
 # Run eslint
 echo $PATH
 
-echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
-. ~/.bashrc
-mkdir ~/local
-mkdir ~/node-latest-install
-cd ~/node-latest-install
-curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
-./configure --prefix=~/local
-make install # ok, fine, this step probably takes more than 30 seconds...
-curl https://www.npmjs.org/install.sh | sh
-
+sudo apt-get install nodejs
+sudo apt-get install npm
 
 cd src/pfe/portal
 npm install
