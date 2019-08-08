fcb49701f9807f1c9885b37d973d39b7ba9b35bf
Stephanie <stephanie.cao@ibm.com>
2019-08-08 19:06:59 -0400

    add enableautobuild & disable autobuild test
    Signed-off-by: Stephanie <stephanie.cao@ibm.com>

--- src/pfe/file-watcher/server/test/scripts/unit_cleanup.sh
+++ src/pfe/file-watcher/server/test/scripts/unit_cleanup.sh
@@ -7,7 +7,7 @@ BLUE='\033[0;36m'
 RESET='\033[0m'
 
 echo -e "${BLUE}Removing test projects... ${RESET}\n"
-rm -rf SVTPythonTemplate nodeExpressTemplate microclimateGoTemplate lagomJavaTemplate springJavaTemplate swiftTemplate javaMicroProfileTemplate
+rm -rf SVTPythonTemplate nodeExpressTemplate springJavaTemplate swiftTemplate javaMicroProfileTemplate
 
 echo -e "${BLUE}Removing test log directory... ${RESET}\n"
 rm -rf .logs/
--- src/pfe/file-watcher/server/test/scripts/unit_setup.sh
+++ src/pfe/file-watcher/server/test/scripts/unit_setup.sh
@@ -8,17 +8,13 @@ RESET='\033[0m'
 
 declare -a PROJECT_URLS
 PROJECT_URLS[0]="https://github.com/microclimate-dev2ops/SVTPythonTemplate"
-PROJECT_URLS[1]="https://github.com/microclimate-dev2ops/microclimateGoTemplate"
-PROJECT_URLS[2]="https://github.com/microclimate-dev2ops/lagomJavaTemplate"
-PROJECT_URLS[3]="https://github.com/microclimate-dev2ops/springJavaTemplate"
-PROJECT_URLS[4]="https://github.com/microclimate-dev2ops/javaMicroProfileTemplate"
-PROJECT_URLS[5]="https://github.com/microclimate-dev2ops/nodeExpressTemplate"
-PROJECT_URLS[6]="https://github.com/microclimate-dev2ops/swiftTemplate"
+PROJECT_URLS[1]="https://github.com/microclimate-dev2ops/springJavaTemplate"
+PROJECT_URLS[2]="https://github.com/microclimate-dev2ops/javaMicroProfileTemplate"
+PROJECT_URLS[3]="https://github.com/microclimate-dev2ops/nodeExpressTemplate"
+PROJECT_URLS[4]="https://github.com/microclimate-dev2ops/swiftTemplate"
 
-echo ${#PROJECT_URLS[@]}
 
 for PROJECT_URL in ${PROJECT_URLS[@]}; do
-    echo -e "${BLUE}Cloning $PROJECT_URLS. ${RESET}\n"
-    # echo $PROJECT_URL
+    echo -e "${BLUE}Cloning $PROJECT_URL. ${RESET}\n"
     git clone $PROJECT_URL
 done
