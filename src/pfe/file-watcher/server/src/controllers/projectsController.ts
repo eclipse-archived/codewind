22a4920fc4594b4dd9522d4eaf8666fd762dd2e7
Andrew Mak <makandre@ca.ibm.com>
2019-08-09 13:18:19 -0400

    missing project ID
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>

--- src/pfe/file-watcher/server/src/controllers/projectsController.ts
+++ src/pfe/file-watcher/server/src/controllers/projectsController.ts
@@ -179,6 +179,7 @@ export async function createProject(req: ICreateProjectParams): Promise<ICreateP
         logger.logProjectInfo("Creating project " + projectLocation, projectID, projectName);
 
         projInfo = {
+            projectID: projectID,
             projectType: projectType,
             location: projectLocation,
             language: req.language
