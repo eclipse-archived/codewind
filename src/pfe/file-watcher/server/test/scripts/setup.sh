db03b894ef50fa186c547aa5f6f2121f23530fbf
Maysun J Faisal <maysun.j.faisal@ibm.com>
2019-08-13 18:01:05 -0400

    Update Test Script Commit 1
    Signed-off-by: Maysun J Faisal <maysun.j.faisal@ibm.com>

--- src/pfe/file-watcher/server/test/scripts/setup.sh
+++ src/pfe/file-watcher/server/test/scripts/setup.sh
@@ -26,11 +26,32 @@ EOF
 # 1 Build Codewind images
 # 2 Run Codewind containers
 function install {
-    $CW_DIR/run.sh
+    if [ $TEST_TYPE == "local" ]; then
+        $CW_DIR/run.sh
+        if [[ $? -ne 0 ]]; then
+            echo -e "${RED}Codewind setup has failed. ${RESET}\n"
+            exit 1
+        fi
+    elif [ $TEST_TYPE == "kube" ]; then
+        # Create Codewind workspace with Che API
+        echo -e "${BLUE}Creating Che Codewind Workspace ${RESET}\n"
+        curl  --header "Content-Type: application/json" --request POST --data @./resources/workspace.json http://che-$NAMESPACE.$CLUSTER_IP.nip.io/api/workspace/devfile?start-after-create=true
 
-    if [[ $? -ne 0 ]]; then
-        echo -e "${RED}Setup is failed. ${RESET}\n"
-        exit 1
+        # Wait until the Codewind pod is up and running
+        POD_RUNNING=0
+        while [ $POD_RUNNING -eq 0 ]; do
+	        RESULT="$( kubectl get po --selector=app=codewind-pfe )"
+	        if [[ $RESULT = *"Running"* ]]; then
+		        POD_RUNNING=1
+                echo -e "${GREEN}Codewind pod is running... ${RESET}\n"
+            elif [[ -z $RESULT ]]; then
+                echo -e "${BLUE}No resources found. Waiting for Codewind pod to be created... ${RESET}\n"
+	        elif [[ $RESULT = *"Failure"* || $RESULT = *"Unknown"* || $RESULT = *"ImagePullBackOff"* || $RESULT = *"CrashLoopBackOff"* || $RESULT = *"PostStartHookError"* ]]; then
+		        echo -e "${RED}Codewind pod failed to start ${RESET}\n"
+		        exit 1
+	        fi
+	        sleep 1
+        done
     fi
 }
 
@@ -38,11 +59,39 @@ function install {
 # 1 Remove Codewind and Codewind app containers
 # 2 Remove Codewind and Codewind app images
 function uninstall {
-    $CW_DIR/stop.sh
+    if [ $TEST_TYPE == "local" ]; then
+        $CW_DIR/stop.sh
 
-    if [[ $? -ne 0 ]]; then
-        echo -e "${RED}Cleanup is failed. ${RESET}\n"
-        exit 1
+        if [[ $? -ne 0 ]]; then
+            echo -e "${RED}Cleanup is failed. ${RESET}\n"
+            exit 1
+        fi
+    elif [ $TEST_TYPE == "kube" ]; then
+        # Get the Codewind Workspace ID
+        CW_POD="$( kubectl get po --selector=app=codewind-pfe --show-labels | tail -n 1 )"
+        echo -e "${BLUE}Codewind Pod: $CW_POD ${RESET}\n"
+        if [[ $CW_POD =~ codewindWorkspace=.*, ]]; then
+            RE_RESULT=${BASH_REMATCH}
+            WORKSPACE_ID=$(echo $RE_RESULT | cut -d "=" -f 2 | cut -d "," -f 1)
+        else
+            echo -e "${RED}Unable to parse kubectl output. Cannot uninstall Codewind. Please uninstall manually... ${RESET}\n"
+            exit 1
+        fi
+        echo -e "${BLUE}Codewind Workspace ID: $WORKSPACE_ID ${RESET}\n"
+
+        # Stop the Codewind Workspace
+        echo -e "${BLUE}Stopping the Codewind Workspace ${RESET}\n"
+        curl --request DELETE http://che-$NAMESPACE.$CLUSTER_IP.nip.io/api/workspace/$WORKSPACE_ID/runtime
+
+        # We must wait for the workspace to stop before removing it, otherwise the workspace removal fails
+        echo -e "${BLUE}Sleeping for 10s to allow the workspace to stop before removing it ${RESET}\n"
+        sleep 10
+
+        # Remove the Codewind Workspace
+        echo -e "${BLUE}Removing the Codewind Workspace ${RESET}\n"
+        curl --request DELETE http://che-$NAMESPACE.$CLUSTER_IP.nip.io/api/workspace/$WORKSPACE_ID
+
+        echo -e "${GREEN}Codewind should be removed momentarily... ${RESET}\n"
     fi
 }
 
@@ -73,14 +122,32 @@ while getopts "t:f:h" OPTION; do
     esac
 done
 
-# Chekc if the mandatory arguments have been set up
+# Check if the mandatory arguments have been set up
 if [[ (-z $TEST_TYPE) || (-z $SETUP_FUNCTION) ]]; then
     echo -e "${RED}Mandatory arguments are not set up. ${RESET}\n"
     usage
     exit 1
 fi
 
-if [[ ($SETUP_FUNCTION == "install") ]];then
+# Log in to the OKD cluster with default credentials
+if [[ $TEST_TYPE == "kube" ]]; then
+    # Check if the mandatory arguments have been set up
+    if [[ (-z $NAMESPACE) || (-z $CLUSTER_IP) ]]; then
+        echo -e "${RED}Mandatory arguments NAMESPACE & CLUSTER_IP are not set up. ${RESET}\n"
+        echo -e "${RED}Please export variables NAMESPACE & CLUSTER_IP to run the Kube tests. ${RESET}\n"
+        exit 1
+    fi
+
+    oc login $CLUSTER_IP:8443 -u ocadmin -p ocadmin
+    if [[ $? -eq 0 ]]; then
+        echo -e "${GREEN}Successfully logged into the OKD cluster ${RESET}\n"
+    else
+        echo -e "${RED}Failed to log into the OKD cluster ${RESET}\n"
+        exit 1
+    fi
+fi
+
+if [[ $SETUP_FUNCTION == "install" ]];then
     install
 elif [[ ($SETUP_FUNCTION == "uninstall") ]];then
     uninstall
