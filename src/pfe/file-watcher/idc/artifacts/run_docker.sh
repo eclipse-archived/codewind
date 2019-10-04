9a1eb05eac5f515ffac3a15d9251b293ff05489f
ssh24 <sakib@ibm.com>
2019-10-04 14:10:51 -0400

    Fix liberty run docker script with correct app directory
    Signed-off-by: ssh24 <sakib@ibm.com>

--- src/pfe/file-watcher/idc/artifacts/run_docker.sh
+++ src/pfe/file-watcher/idc/artifacts/run_docker.sh
@@ -34,6 +34,7 @@ export ARTIFACTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # App dir
 export APPDIR=`pwd`
+APP_DIRECTORY="$APPDIR"
 
 export APPNAME=$(dirname "$APPDIR")
 
@@ -67,7 +68,7 @@ if [[ $MICROCLIMATE_WS_ORIGIN &&  "$APPDIR" == '/codewind-workspace'* ]]
 			OUTPUT_DOCKER_RUN="$(docker run -dt --entrypoint "/home/default/artifacts/new_entrypoint.sh" --name $CONTAINER_NAME -v "$LOGSDIR":$HOME/logs --network=codewind_network $PORT_MAPPING_PARAMS $CONTAINER_IMAGE_NAME)"
 			if [ $? -eq 0 ]; then
 				echo -e "Copying over source files"
-				docker cp "$APPDIR" $CONTAINER_NAME:$HOME/app
+				docker cp "$APP_DIRECTORY" $CONTAINER_NAME:$HOME/app
 			fi
 			echo "${OUTPUT_DOCKER_RUN}"
 		else
@@ -87,7 +88,7 @@ if [[ $MICROCLIMATE_WS_ORIGIN &&  "$APPDIR" == '/codewind-workspace'* ]]
 			OUTPUT_DOCKER_RUN="$(docker run -dt --name $CONTAINER_NAME -v "$LOGSDIR":$HOME/logs $PORT_MAPPING_PARAMS $CONTAINER_IMAGE_NAME)"
 			if [ $? -eq 0 ]; then
 				echo -e "Copying over source files"
-				docker cp "$APPDIR" $CONTAINER_NAME:$HOME/app
+				docker cp "$APP_DIRECTORY" $CONTAINER_NAME:$HOME/app
 			fi
 			echo "${OUTPUT_DOCKER_RUN}"
 		else
