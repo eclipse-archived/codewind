304e17484a67c20d4c8eb7978ed673858762df9b
Andrew Mak <makandre@ca.ibm.com>
2019-10-08 15:36:41 -0400

    update from provider only 1st time (#639)
    * update from provider only 1st time
    
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>
    
    * tweak
    
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>

--- src/pfe/portal/routes/templates.route.js
+++ src/pfe/portal/routes/templates.route.js
@@ -16,6 +16,7 @@ const TemplateError = require('../modules/utils/errors/TemplateError');
 
 const router = express.Router();
 const log = new Logger(__filename);
+let updatePromise = null;
 
 /**
  * API Function to return a list of available templates
@@ -46,7 +47,9 @@ router.get('/api/v1/templates', validateReq, async (req, res, _next) => {
  */
 router.get('/api/v1/templates/repositories', async (req, res, _next) => {
   const user = req.cw_user;
-  await user.templates.updateRepoListWithReposFromProviders();
+  if (!updatePromise)
+    updatePromise = user.templates.updateRepoListWithReposFromProviders();
+  await updatePromise;
   await sendRepositories(req, res, _next);
 });
 
