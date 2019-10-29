f59cc6dc3cea1979ed6886ddd991fdab1360d3ad
ssh24 <sakib@ibm.com>
2019-10-29 14:37:41 -0400

    WIP: fix functional test
    Signed-off-by: ssh24 <sakib@ibm.com>

--- src/pfe/file-watcher/server/test/functional-test/functional.test.ts
+++ src/pfe/file-watcher/server/test/functional-test/functional.test.ts
@@ -74,7 +74,7 @@ describe("PFE - functional test", () => {
 });
 
 function runAllTests(): void {
-  genericSuite.runTest();
+  genericSuite.runTest(true);
   for (const chosenType of projectTypes) {
     if (chosenType === "docker") {
       for (const chosenDocker of dockerProjects) {
