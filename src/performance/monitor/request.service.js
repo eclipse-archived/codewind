2eb64a66bdfc7f7a37e93edaad01c24562cc1ed0
Richard Waller <Richard.Waller@ibm.com>
2019-11-06 19:44:03 +0000

    Clarify code structure
    Signed-off-by: Richard Waller <Richard.Waller@ibm.com>

---
+++ src/performance/monitor/request.service.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+const getResData = async (origin, path) => {
+    const href = `${origin}${path}`;
+    console.log(`[getResData] GET ${href}`);
+    const res = await axios.get(href);
+    // console.log('Received res.data');
+    // console.log(res.data);
+    return res.data;
+};
+
+module.exports = {
+    getResData,
+};
