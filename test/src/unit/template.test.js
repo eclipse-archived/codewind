964527441dd8abbced40626c44813bf6fa9b31dd
Toby Corbin <corbint@uk.ibm.com>
2019-11-08 10:56:30 +0000

    modify default repo
    Signed-off-by: Toby Corbin <corbint@uk.ibm.com>

--- test/src/unit/template.test.js
+++ test/src/unit/template.test.js
@@ -230,7 +230,7 @@ describe('Templates.js', function() {
         describe('when providers list valid repos', function() {
             let templateController;
             const validCodewindRepo = {
-                url: 'https://raw.githubusercontent.com/kabanero-io/codewind-templates/aad4bafc14e1a295fb8e462c20fe8627248609a3/devfiles/index.json',
+                url: 'https://raw.githubusercontent.com/codewind-resources/codewind-templates/aad4bafc14e1a295fb8e462c20fe8627248609a3/devfiles/index.json',
                 description: 'valid Codewind repo',
             };
             before(() => {
@@ -430,7 +430,7 @@ describe('Templates.js', function() {
         describe('repo with name and description in templates.json', () => {
             describe('(<validUrl>, <ValidDesc>, <ValidName>)', function() {
                 it('succeeds, and allows the user to set the name and description', async function() {
-                    const url = 'https://raw.githubusercontent.com/kabanero-io/codewind-templates/master/devfiles/index.json';
+                    const url = 'https://raw.githubusercontent.com/codewind-resources/codewind-templates/master/devfiles/index.json';
                     const func = () => templateController.addRepository(url, 'description', 'name', false);
                     await (func().should.not.be.rejected);
                     templateController.repositoryList.should.containSubset([{ ...sampleRepos.codewind,
@@ -442,7 +442,7 @@ describe('Templates.js', function() {
             });
             describe('(repo with templates.json, <validUrl>, <NoDesc>, <NoName>)', function() {
                 it('succeeds, and gets the name and description from templates.json', async function() {
-                    const url = 'https://raw.githubusercontent.com/kabanero-io/codewind-templates/master/devfiles/index.json';
+                    const url = 'https://raw.githubusercontent.com/codewind-resources/codewind-templates/master/devfiles/index.json';
                     const func = () => templateController.addRepository(url, '', '', false);
                     await (func().should.not.be.rejected);
                     templateController.repositoryList.should.containSubset([{ ...sampleRepos.codewind, protected: false }]);
