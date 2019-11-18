4deb7c2b22eaa693435ae51944f9983c848fa3bb
Richard Waller <Richard.Waller@ibm.com>
2019-11-18 11:42:26 +0000

    Inject package into liberty projects
    Signed-off-by: Richard Waller <Richard.Waller@ibm.com>

--- test/src/unit/modules/MetricsService.test.js
+++ test/src/unit/modules/MetricsService.test.js
@@ -10,19 +10,24 @@
 *******************************************************************************/
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const deepEqualInAnyOrder = require('deep-equal-in-any-order');
 const fs = require('fs-extra');
 const path = require('path');
 const rewire = require('rewire');
+const xml2js = require('xml2js');
 
 const metricsService = rewire('../../../../src/pfe/portal/modules/MetricsService');
 
 chai.use(chaiAsPromised);
+chai.use(deepEqualInAnyOrder);
 chai.should();
 
 describe('MetricsService.js', () => {
-    const projectDir = path.join('.', 'src', 'unit');
+    const projectDir = path.join('.', 'src', 'unit', 'modules');
+
+    // nodejs
     const pathToPackageJson = path.join(projectDir, 'package.json');
-    const contentsOfOriginalPackageJson = {
+    const originalPackageJson = {
         /* eslint-disable quote-props, quotes, comma-dangle */
         "name": "node",
         "version": "1.0.0",
@@ -38,7 +43,7 @@ describe('MetricsService.js', () => {
         /* eslint-enable quote-props, quotes, comma-dangle */
     };
 
-    const contentsOfMetricsInjectedPackageJson = {
+    const expectedPackageJson = {
         /* eslint-disable quote-props, quotes, comma-dangle */
         "name": "node",
         "version": "1.0.0",
@@ -56,62 +61,228 @@ describe('MetricsService.js', () => {
         /* eslint-enable quote-props, quotes, comma-dangle */
     };
 
+    // liberty
+    const pathToJvmOptions = path.join(projectDir, 'src', 'main', 'liberty', 'config', 'jvm.options');
+    const originalJvmOptions = 'foobar';
+    const expectedJvmOptions = `${originalJvmOptions}\n-javaagent:resources/javametrics-agent.jar`;
+
+    const pathToTestPomXml = path.join(projectDir, 'pom.xml');
+
+    const pathToTestResourcesDir = path.join('.', 'resources', 'metricsService');
+    const pathToOriginalPomXml = path.join(pathToTestResourcesDir, 'pom without collector.xml');
+    const pathToExpectedPomXml = path.join(pathToTestResourcesDir, 'pom with collector.xml');
+    let originalPomXml;
+    let expectedPomXml;
+
+    before(async() => {
+        originalPomXml = await readXml(pathToOriginalPomXml);
+        expectedPomXml = await readXml(pathToExpectedPomXml);
+    });
+
     describe('injectMetricsCollectorIntoProject(projectLanguage, projectDir)', () => {
-        beforeEach(() => {
-            fs.writeJSONSync(pathToPackageJson, contentsOfOriginalPackageJson);
-        });
-        afterEach(() => {
-            fs.unlinkSync(pathToPackageJson);
-        });
         describe(`('nodejs', <goodProjectDir>)`, () => {
+            beforeEach(() => {
+                fs.writeJSONSync(pathToPackageJson, originalPackageJson);
+            });
+            afterEach(() => {
+                fs.unlinkSync(pathToPackageJson);
+            });
             it(`injects metrics collector into the project's package.json`, async() => {
                 await metricsService.injectMetricsCollectorIntoProject('nodejs', projectDir);
 
-                fs.readJSONSync(pathToPackageJson).should.deep.equal(contentsOfMetricsInjectedPackageJson);
+                fs.readJSONSync(pathToPackageJson).should.deep.equal(expectedPackageJson);
+            });
+        });
+        describe(`('liberty', <goodProjectDir>)`, () => {
+            beforeEach(() => {
+                fs.writeFileSync(pathToTestPomXml, fs.readFileSync(pathToOriginalPomXml));
+                fs.outputFileSync(pathToJvmOptions, originalJvmOptions);
+            });
+            afterEach(() => {
+                fs.unlinkSync(pathToTestPomXml);
+                fs.unlinkSync(pathToJvmOptions);
+            });
+            it(`injects metrics collector into the project's jvm.options and pom.xml`, async() => {
+                const funcToTest = metricsService.__get__('injectMetricsCollectorIntoProject');
+                await funcToTest('liberty', projectDir);
+
+                const outputPomXml = await readXml(pathToTestPomXml);
+                outputPomXml.should.deep.equalInAnyOrder(expectedPomXml);
+
+                const outputJvmOptions = fs.readFileSync(pathToJvmOptions, 'utf8');
+                outputJvmOptions.should.equal(expectedJvmOptions);
             });
         });
         describe(`('unsupportedLanguage', <goodProjectDir>)`, () => {
+            beforeEach(() => {
+                fs.writeJSONSync(pathToPackageJson, originalPackageJson);
+            });
+            afterEach(() => {
+                fs.unlinkSync(pathToPackageJson);
+            });
             it(`throws a useful error`, () => {
                 const funcToTest = () => metricsService.injectMetricsCollectorIntoProject('unsupportedLanguage', projectDir);
                 return funcToTest().should.be.rejectedWith(`'unsupportedLanguage' is not a supported language`);
             });
         });
     });
-    describe('injectMetricsCollectorIntoNodeProject(projectDir)', () => {
-        describe('package.json does not have metrics injection code', () => {
-            before(() => {
-                fs.writeJSONSync(pathToPackageJson, contentsOfOriginalPackageJson);
-            });
-            after(() => {
-                fs.unlinkSync(pathToPackageJson);
-            });
-            it(`injects metrics collector into the project's package.json`, async() => {
-                const funcToTest = metricsService.__get__('injectMetricsCollectorIntoNodeProject');
-                await funcToTest(projectDir);
+    describe('nodejs', () => {
+        describe('injectMetricsCollectorIntoNodeProject(projectDir)', () => {
+            describe('package.json does not have metrics injection code', () => {
+                before(() => {
+                    fs.writeJSONSync(pathToPackageJson, originalPackageJson);
+                });
+                after(() => {
+                    fs.unlinkSync(pathToPackageJson);
+                });
+                it(`injects metrics collector into the project's package.json`, async() => {
+                    const funcToTest = metricsService.__get__('injectMetricsCollectorIntoNodeProject');
+                    await funcToTest(projectDir);
 
-                fs.readJSONSync(pathToPackageJson).should.deep.equal(contentsOfMetricsInjectedPackageJson);
+                    fs.readJSONSync(pathToPackageJson).should.deep.equal(expectedPackageJson);
+                });
+            });
+            describe('package.json already has metrics injection code', () => {
+                before(() => {
+                    fs.writeJSONSync(pathToPackageJson, expectedPackageJson);
+                });
+                after(() => {
+                    fs.unlinkSync(pathToPackageJson);
+                });
+                it(`injects metrics collector into the project's package.json`, async() => {
+                    const funcToTest = metricsService.__get__('injectMetricsCollectorIntoNodeProject');
+                    await funcToTest(projectDir);
+
+                    fs.readJSONSync(pathToPackageJson).should.deep.equal(expectedPackageJson);
+                });
             });
         });
-        describe('package.json already has metrics injection code', () => {
-            before(() => {
-                fs.writeJSONSync(pathToPackageJson, contentsOfMetricsInjectedPackageJson);
-            });
-            after(() => {
-                fs.unlinkSync(pathToPackageJson);
-            });
-            it(`injects metrics collector into the project's package.json`, async() => {
-                const funcToTest = metricsService.__get__('injectMetricsCollectorIntoNodeProject');
-                await funcToTest(projectDir);
-
-                fs.readJSONSync(pathToPackageJson).should.deep.equal(contentsOfMetricsInjectedPackageJson);
+        describe('getNewContentsOfPackageJson(originalContents)', () => {
+            it(`returns an object representing a package.json injected with metrics collector`, () => {
+                const funcToTest = metricsService.__get__('getNewContentsOfPackageJson');
+                const output = funcToTest(originalPackageJson);
+                output.should.deep.equal(expectedPackageJson);
             });
         });
     });
-    describe('getNewContentsOfPackageJson(oldContentsOfPackageJson)', () => {
-        it(`returns an object representing a package.json injected with metrics collector`, () => {
-            const funcToTest = metricsService.__get__('getNewContentsOfPackageJson');
-            const output = funcToTest(contentsOfOriginalPackageJson);
-            output.should.deep.equal(contentsOfMetricsInjectedPackageJson);
+
+    describe('liberty', () => {
+        describe('injectMetricsCollectorIntoLibertyProject(projectDir)', () => {
+            beforeEach(() => {
+                fs.writeFileSync(pathToTestPomXml, fs.readFileSync(pathToOriginalPomXml));
+                fs.outputFileSync(pathToJvmOptions, originalJvmOptions);
+            });
+            afterEach(() => {
+                fs.unlinkSync(pathToTestPomXml);
+                fs.unlinkSync(pathToJvmOptions);
+            });
+            it(`injects metrics collector into the project's jvm.options and pom.xml`, async() => {
+                const funcToTest = metricsService.__get__('injectMetricsCollectorIntoLibertyProject');
+                await funcToTest(projectDir);
+
+                const outputPomXml = await readXml(pathToTestPomXml);
+                outputPomXml.should.deep.equalInAnyOrder(expectedPomXml);
+
+                const outputJvmOptions = fs.readFileSync(pathToJvmOptions, 'utf8');
+                outputJvmOptions.should.equal(expectedJvmOptions);
+            });
+        });
+
+        describe('injectMetricsCollectorIntoPomXml(pathToPomXml)', () => {
+            beforeEach(() => {
+                fs.writeFileSync(pathToTestPomXml, fs.readFileSync(pathToOriginalPomXml));
+            });
+            afterEach(() => {
+                fs.unlinkSync(pathToTestPomXml);
+            });
+            it(`injects metrics collector into the project's pom.xml`, async() => {
+                const funcToTest = metricsService.__get__('injectMetricsCollectorIntoPomXml');
+                await funcToTest(pathToTestPomXml);
+
+                const outputPomXml = await readXml(pathToTestPomXml);
+                outputPomXml.should.deep.equalInAnyOrder(expectedPomXml);
+            });
+        });
+
+        describe('injectMetricsCollectorIntoJvmOptions(pathToJvmOptions)', () => {
+            beforeEach(() => {
+                fs.outputFileSync(pathToJvmOptions, originalJvmOptions);
+            });
+            afterEach(() => {
+                fs.unlinkSync(pathToJvmOptions);
+            });
+            it(`injects metrics collector into the project's pom.xml`, async() => {
+                const funcToTest = metricsService.__get__('injectMetricsCollectorIntoJvmOptions');
+                await funcToTest(pathToJvmOptions);
+
+                const outputJvmOptions = fs.readFileSync(pathToJvmOptions, 'utf8');
+                outputJvmOptions.should.equal(expectedJvmOptions);
+            });
+        });
+
+        describe('getNewContentsOfJvmOptions(originalContents)', () => {
+            it('returns an object representing a pom.xml injected with metrics collector', () => {
+                const originalJvmOptions = 'foobar';
+                const funcToTest = metricsService.__get__('getNewContentsOfJvmOptions');
+                const output = funcToTest(originalJvmOptions);
+                output.should.equal(expectedJvmOptions);
+            });
+        });
+
+        describe('getNewContentsOfPomXml(originalContents)', () => {
+            it('returns an object representing a pom.xml injected with metrics collector', () => {
+                const funcToTest = metricsService.__get__('getNewContentsOfPomXml');
+                const output = funcToTest(originalPomXml);
+                output.should.deep.equalInAnyOrder(expectedPomXml);
+            });
+        });
+
+        describe('getNewPomXmlDependencies(originalDependencies)', () => {
+            it(`returns an object representing pom.xml dependencies injected with metrics collector`, () => {
+                const funcToTest = metricsService.__get__('getNewPomXmlDependencies');
+                const originalDependencies = [];
+                const output = funcToTest(originalDependencies);
+                output.should.have.deep.members([
+                    ...originalDependencies,
+                    {
+                        groupId: [ 'com.ibm.runtimetools' ],
+                        artifactId: [ 'javametrics-dash' ],
+                        version: [ '[1.2,2.0)' ],
+                        scope: [ 'provided' ],
+                        type: [ 'war' ],
+                    },
+                ]);
+            });
+        });
+        describe('getNewPomXmlBuildPluginExecutions(originalBuildPluginExecutions)', () => {
+            it(`returns an object representing pom.xml build plugin executions that are injected with metrics collector`, () => {
+                const funcToTest = metricsService.__get__('getNewPomXmlBuildPluginExecutions');
+                const originalBuildPluginExecutions = [];
+                const output = funcToTest(originalBuildPluginExecutions);
+                output.should.have.deep.members([
+                    ...originalBuildPluginExecutions,
+                    {
+                        id: [ 'copy-javametrics-dash' ],
+                        phase: [ 'package' ],
+                        goals: [ { goal: [ 'copy-dependencies' ] } ],
+                        configuration: [
+                            {
+                                stripVersion: [ 'true' ],
+                                outputDirectory: [
+                                    '${project.build.directory}/liberty/wlp/usr/servers/defaultServer/dropins',
+                                ],
+                                includeArtifactIds: [ 'javametrics-dash' ],
+                            },
+                        ],
+                    },
+                ]);
+            });
         });
     });
 });
+
+async function readXml(pathToXmlFile) {
+    const xmlFileData = fs.readFileSync(pathToXmlFile);
+    const xmlAsJson = await xml2js.parseStringPromise(xmlFileData);
+    return xmlAsJson;
+}
