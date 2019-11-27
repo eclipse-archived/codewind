8e9fbb896993586c7512db4425932dc66bfe4ba6
Andrew Mak <makandre@ca.ibm.com>
2019-11-27 09:56:36 -0500

    pushLocal.sh script
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>

---
+++ src/pfe/portal/pushLocal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker cp config/. codewind-pfe:/portal/config
+docker cp middleware/. codewind-pfe:/portal/middleware
+docker cp modules/. codewind-pfe:/portal/modules
+docker cp routes/. codewind-pfe:/portal/routes
+docker cp server.js codewind-pfe:/portal/server.js
