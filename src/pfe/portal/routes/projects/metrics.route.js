9f5f744565fe5daa14119bd344d618042c6fccff
Toby Corbin <corbint@uk.ibm.com>
2019-12-02 14:16:44 +0000

    trigger build on metrics injection (#1313)
    * trigger build on metrics injection
    
    Signed-off-by: Toby Corbin <corbint@uk.ibm.com>
    
    * Fix file not being deleted on rename + other `sync/end` bugs (#1310)
    
    * Add unit test for remote bind functions
    * Fix file not being deleted after rename and blank spaces in file name
    * Only run deleteFiles function if file array is populated
    
    Signed-off-by: James Wallis <james.wallis1@ibm.com>
    
    * trigger build on metrics injection
    
    Signed-off-by: Toby Corbin <corbint@uk.ibm.com>
    
    * remove await from initial call to copy
    
    Signed-off-by: Toby Corbin <corbint@uk.ibm.com>
    
    * move move build logic
    
    Signed-off-by: Toby Corbin <corbint@uk.ibm.com>
    
    * move build logic
    
    Signed-off-by: Toby Corbin <corbint@uk.ibm.com>
    
    * move build logic
    
    Signed-off-by: Toby Corbin <corbint@uk.ibm.com>

--- src/pfe/portal/routes/projects/metrics.route.js
+++ src/pfe/portal/routes/projects/metrics.route.js
@@ -16,6 +16,9 @@ const ProjectError = require('../../modules/utils/errors/ProjectError');
 const ProjectMetricsError = require('../../modules/utils/errors/ProjectMetricsError');
 const Project = require('../../modules/Project');
 const { validateReq } = require('../../middleware/reqValidator');
+const metricsService = require('../../modules/MetricsService');
+const cwUtils = require('../../modules/utils/sharedFunctions');
+const path = require('path');
 const router = express.Router();
 const log = new Logger(__filename);
 
@@ -187,7 +190,9 @@ router.post('/api/v1/projects/:id/metrics/inject', validateReq, async function (
       injectMetrics: injectMetrics
     });
 
-    user.buildProject(project, "build");
+    await metricsService.injectMetricsCollectorIntoProject(project.projectType, path.join(project.workspace, project.directory));
+    // We now need to synch files in to the build container
+    copyProjectToBuild(project, user);
 
     res.sendStatus(202);
 
@@ -197,6 +202,22 @@ router.post('/api/v1/projects/:id/metrics/inject', validateReq, async function (
   }
 });
 
+async function copyProjectToBuild(project, user){
+  const globalProjectPath = path.join(project.workspace, project.name);
+  const projectRoot = cwUtils.getProjectSourceRoot(project);
+  if (project.buildStatus != "inProgress") {
+    await cwUtils.copyProjectContents(
+      project,
+      globalProjectPath,
+      projectRoot
+    );
+    await user.buildProject(project, "build");
+  } else {
+    // if a build is in progress, wait 5 seconds and try again
+    await cwUtils.timeout(5000)
+    await copyProjectToBuild(project);
+  }
+}
 
 /**
 * Updates the description of a specific load-test run on a specified project
