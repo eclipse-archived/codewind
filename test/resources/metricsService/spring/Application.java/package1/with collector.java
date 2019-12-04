0463e06e28056419bcd7a165136cc7e6d3ca0298
Matthew Colegate <colegate@uk.ibm.com>
2019-12-04 12:25:58 +0000

    Improvements to javametrics injection & tests
    Signed-off-by: Matthew Colegate <colegate@uk.ibm.com>

--- test/resources/metricsService/spring/Application.java/package1/with collector.java
+++ test/resources/metricsService/spring/Application.java/package1/with collector.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"application", "com.ibm.javametrics.spring"})
+@ComponentScan(basePackages = {"com.ibm.javametrics.codewind.spring", "application"})
 public class SBApplication {
 
     public static void main(String[] args) {
--- test/resources/metricsService/spring/Application.java/package3/without collector.java
+++ test/resources/metricsService/spring/Application.java/package3/without collector.java
@@ -1,11 +1,11 @@
-package application2;
+package application3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.ibm.javametrics.codewind.spring", "application2"})
+@ComponentScan(basePackages = {"application3"})
 public class SBApplication {
 
     public static void main(String[] args) {
