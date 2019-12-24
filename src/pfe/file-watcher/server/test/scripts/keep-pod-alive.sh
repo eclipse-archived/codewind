2f0c24355951f0b58128a8db795275d38278a7cc
ssh24 <sakib@ibm.com>
2019-12-24 17:16:54 -0500

    Transfer loop into a bash script and kill process
    Signed-off-by: ssh24 <sakib@ibm.com>

---
+++ src/pfe/file-watcher/server/test/scripts/keep-pod-alive.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#*******************************************************************************
+# Copyright (c) 2019 IBM Corporation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v2.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v20.html
+#
+# Contributors:
+#     IBM Corporation - initial API and implementation
+#*******************************************************************************
+
+INTERVAL="30s"
+
+while true;
+do
+    echo "Keeping PFE pod/container alive every $INTERVAL ...";
+    sleep $INTERVAL;
+done
