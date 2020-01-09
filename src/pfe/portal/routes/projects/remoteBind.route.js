c934ea97912e940064f572e16b2ceaa322c542bd
Mark Cornaia <mark.cornaia@uk.ibm.com>
2020-01-09 07:34:21 +0000

    Revert "Return error response if unbind fails (#1630)"
    This reverts commit 3bf41f38f8aaa1d6550ea3696db8c5b7ee97704a.

--- src/pfe/portal/routes/projects/remoteBind.route.js
+++ src/pfe/portal/routes/projects/remoteBind.route.js
@@ -485,7 +485,6 @@ router.post('/api/v1/projects/:id/unbind', validateReq, async function (req, res
     }
     user.uiSocket.emit('projectDeletion', data);
     log.error(`Error deleting project: ${util.inspect(data)}`);
-    res.status(500).send(data.error);
   }
 });
 
