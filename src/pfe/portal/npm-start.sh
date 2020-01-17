729973b76c4cb4312f2ac4894e1d692e2b5118c0
James Wallis <james.wallis1@ibm.com>
2020-01-17 21:02:15 +0000

    Add script to get code coverage from codewind-pfe container, add to test.sh to get coverage when testing
    Signed-off-by: James Wallis <james.wallis1@ibm.com>

--- src/pfe/portal/npm-start.sh
+++ src/pfe/portal/npm-start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+NPM_CMD='npm'
+if [ ! -z "$ENABLE_CODE_COVERAGE" ] && [ "$ENABLE_CODE_COVERAGE" == "true" ]; then
+  NPM_CMD='npx nyc@15.0.0 npm'
+  echo "Running with code coverage enabled through nyc ($NPM_CMD)"
+fi
+
 if [ ! -z "$NODE_ENV" ] && [ "$NODE_ENV" == "development" ]; then
   echo "Found node environment set to dev. Running in development mode."
-  npm run dev
+  $NPM_CMD run dev
 else
   echo "Found node environment set to prod. Running in production mode."
-  npm run prod
+  $NPM_CMD run prod
 fi
