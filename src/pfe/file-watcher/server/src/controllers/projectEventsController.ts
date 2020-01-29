ac8c2ff93d7487a3be37a2c655f0bd1f40e46895
ssh24 <sakib@ibm.com>
2020-01-29 17:14:53 -0500

    Emit empty message instead of calculate difference
    Signed-off-by: ssh24 <sakib@ibm.com>

--- src/pfe/file-watcher/server/src/controllers/projectEventsController.ts
+++ src/pfe/file-watcher/server/src/controllers/projectEventsController.ts
@@ -230,7 +230,7 @@ export async function updateProjectForNewChange(projectID: string, timestamp: nu
                                 handler: projectHandler
                             };
                             // for update operation
-                            await statusController.updateProjectStatus(statusController.STATE_TYPES.buildState, projectID, statusController.BuildState.inProgress, "action.calculateDifference");
+                            await statusController.updateProjectStatus(statusController.STATE_TYPES.buildState, projectID, statusController.BuildState.inProgress, "");
                             await projectsController.addProjectToBuildQueue(project);
                         } else {
                             logger.logProjectInfo("Project "  + projectID + " build is in progress, set build request flag to true", projectID);
