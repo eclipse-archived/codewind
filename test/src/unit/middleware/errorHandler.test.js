e2853da3af81d03f68c8a0bdd81bc133c06a92c9
Richard Waller <Richard.Waller@ibm.com>
2020-01-29 11:17:58 +0000

    chore: log error stack traces
    Signed-off-by: Richard Waller <Richard.Waller@ibm.com>

--- test/src/unit/middleware/errorHandler.test.js
+++ test/src/unit/middleware/errorHandler.test.js
@@ -1,25 +1,16 @@
-require('mocha-sinon');
 const rewire = require('rewire');
 const chai = require('chai');
 
-const sinonChai = require('sinon-chai');
 const errorHandler = rewire('../../../../src/pfe/portal/middleware/errorHandler');
 
-const logError = errorHandler.__get__('logError');
-const log = errorHandler.__get__('log');
+const generateLogMsg = errorHandler.__get__('generateLogMsg');
 
-chai.use(sinonChai);
 chai.should();
 
 describe('logError(err)', function() {
-    it('logs the correct error info to console', function() {
-        const spy = this.sinon.spy(log, 'error');
-        logError(new TypeError('errMsg'));
-        spy.should.have.been.calledWith({
-            error: {
-                name: 'TypeError',
-                message: 'errMsg',
-            },
-        });
+    it('returns the correct log message', function() {
+        const err = new TypeError('errMsg');
+        const output = generateLogMsg(err);
+        output.should.deep.equal(err);
     });
 });
