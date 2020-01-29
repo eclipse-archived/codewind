9f434f3afd3a9aebe4373ac27625b63a00f01e03
Richard Waller <Richard.Waller@ibm.com>
2020-01-29 11:17:58 +0000

    chore: log error stack traces
    Signed-off-by: Richard Waller <Richard.Waller@ibm.com>

--- src/pfe/portal/middleware/errorHandler.js
+++ src/pfe/portal/middleware/errorHandler.js
@@ -3,18 +3,17 @@ const Logger = require('../modules/utils/Logger');
 const log = new Logger(__filename);
 
 function handleErrors(err, req, res, _next) {
-  logError(err);
+  log.error(generateLogMsg(err));
   const statusCode = err.statusCode || 500;
   res.status(statusCode).send(err.message);
 }
 
-function logError(err) {
-  log.error({
-    error: {
-      name: err.name,
-      message: err.message,
-    },
-  });
+function generateLogMsg(err) {
+  const errorsToLogSuccintly = ['ValidationError'];
+  if (errorsToLogSuccintly.includes(err.name)) {
+    return `${err.name}: ${err.message}`;
+  } 
+  return err;
 }
 
 module.exports = {
