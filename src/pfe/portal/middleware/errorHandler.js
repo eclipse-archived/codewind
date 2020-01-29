620dea5bcdfa1345e8161e5fc54fbaef38b0d717
Richard Waller <Richard.Waller@ibm.com>
2020-01-29 14:57:26 +0000

    chore: improve route validation err logging
    Signed-off-by: Richard Waller <Richard.Waller@ibm.com>

--- src/pfe/portal/middleware/errorHandler.js
+++ src/pfe/portal/middleware/errorHandler.js
@@ -3,17 +3,9 @@ const Logger = require('../modules/utils/Logger');
 const log = new Logger(__filename);
 
 function handleErrors(err, req, res, _next) {
-  log.error(generateLogMsg(err));
+  log.error(err.customLogMsg || err);
   const statusCode = err.statusCode || 500;
   res.status(statusCode).send(err.message);
-}
-
-function generateLogMsg(err) {
-  const errorsToLogSuccintly = ['ValidationError'];
-  if (errorsToLogSuccintly.includes(err.name)) {
-    return `${err.name}: ${err.message}`;
-  } 
-  return err;
 }
 
 module.exports = {
