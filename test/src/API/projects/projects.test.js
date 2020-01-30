d906f2605a9156063228ad09e4381073c00b2a8d
Richard Waller <richard.lh.waller@gmail.com>
2020-01-30 11:05:49 +0000

    Test projects/{id} API (#1919)
    * test: projects/{id} API
    * test: rename projectlist.test to projects.test
    * test: fix project/:id test
    
    Signed-off-by: Richard Waller <Richard.Waller@ibm.com>

--- test/src/API/projects/projects.test.js
+++ test/src/API/projects/projects.test.js
@@ -11,43 +11,62 @@
 
 const chai = require('chai');
 const path = require('path');
+const chaiResValidator = require('chai-openapi-response-validator');
 
 const projectService = require('../../../modules/project.service');
-const { testTimeout, TEMP_TEST_DIR } = require('../../../config');
+const { testTimeout, TEMP_TEST_DIR, pathToApiSpec } = require('../../../config');
 
+chai.use(chaiResValidator(pathToApiSpec));
 chai.should();
 
-describe('Project-list tests', function() {
-    describe('GET /projects', function() {
+describe('Projects API tests', function() {
+    describe('Success cases for GET /projects and GET /projects/:id', function() {
         let originalProjectIDs;
         let projectID;
         const projectName = `projectlist${Date.now()}`;
         const pathToLocalProject = path.join(TEMP_TEST_DIR, projectName);
 
-        it('should return a list of projectIDs', async function() {
+        it('returns a list of projectIDs', async function() {
             originalProjectIDs = await projectService.getProjectIDs();
             originalProjectIDs.should.be.an('array');
         });
 
-        it('should create a project', async function() {
+        it('creates a project', async function() {
             this.timeout(testTimeout.med);
             projectID = await projectService.createProjectFromTemplate(projectName, 'nodejs', pathToLocalProject);
         });
 
-        it('should return the same list but now including the extra projectID', async function() {
+        it('returns the same list but now including the extra projectID', async function() {
             const ids = await projectService.getProjectIDs();
             ids.should.be.an('array').with.members(originalProjectIDs.concat([projectID]));
         });
+        
+        it('returns the project', async function() {
+            const res = await projectService.getProject(projectID);
+            res.should.satisfyApiSpec;
+            res.body.name.should.equal(projectName);
+        });
 
-        it('should delete the extra project', async function() {
+        it('deletes the extra project', async function() {
             this.timeout(testTimeout.med);
             await projectService.removeProject(pathToLocalProject, projectID);
         });
 
-        it('should return the original list of projectIDs', async function() {
+        it('returns the original list of projectIDs', async function() {
             const ids = await projectService.getProjectIDs();
             ids.should.be.an('array');
             ids.should.deep.equal(originalProjectIDs);
+        });
+    });
+    
+    describe('Fail cases for GET /project/{id}', () => {
+        it('returns 404 when the project does not exist', async function() {
+            this.timeout(testTimeout.short);
+            const idMatchingNoProjects = '00000000-0000-0000-0000-000000000000';
+            const res = await projectService.getProject(idMatchingNoProjects, 404);
+            res.should.have.status(404);
+            res.should.satisfyApiSpec;
+            res.text.should.equal('Not Found');
         });
     });
 });
