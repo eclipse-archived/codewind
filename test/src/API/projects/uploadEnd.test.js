1b4fc35e5f5eec102254b80840e3b9af31cdbbe4
Richard Waller <Richard.Waller@ibm.com>
2020-01-31 10:57:32 +0000

    test, docs: validate 4xx responses against docs
    Signed-off-by: Richard Waller <Richard.Waller@ibm.com>

--- test/src/API/projects/uploadEnd.test.js
+++ test/src/API/projects/uploadEnd.test.js
@@ -383,6 +383,7 @@ describe('Sync tests (POST projects/{id}/upload/end)', () => {
             };
             const res = await projectService.uploadEnd(idMatchingNoProjects, options);
             res.should.have.status(404);
+            res.should.satisfyApiSpec;
         });
     });
 });
