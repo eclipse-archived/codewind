44cdfe4c1809e6bd30d2101deb51cdf30d5abd51
Richard Waller <Richard.Waller@ibm.com>
2020-02-10 17:23:09 +0000

    fix:  check project metrics before responding
    Signed-off-by: Richard Waller <Richard.Waller@ibm.com>

--- src/pfe/portal/routes/projects/remoteBind.route.js
+++ src/pfe/portal/routes/projects/remoteBind.route.js
@@ -254,6 +254,8 @@ async function uploadEnd(req, res) {
       await deletePathsInArray(pathToProj, filesToDelete);
     }
 
+    await project.checkIfMetricsAvailable();
+
     res.sendStatus(200);
     
   } catch (err) {
@@ -267,8 +269,6 @@ async function uploadEnd(req, res) {
       || filesToDelete.length > 0
       || modifiedList.length > 0;
     if (wasProjectChanged) {
-      await project.checkIfMetricsAvailable();
-
       await cwUtils.copyProject(pathToTempProj, path.join(project.workspace, project.directory), getMode(project));
 
       if (project.injectMetrics) {
