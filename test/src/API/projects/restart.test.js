b8b4de9e332d2ccc71157e5d9ce4775a56feb74f
Richard Waller <Richard.Waller@ibm.com>
2020-02-12 16:19:21 +0000

    test, docs, fix: fix restart tests and PFE behaviour
    Signed-off-by: Richard Waller <Richard.Waller@ibm.com>

--- test/src/API/projects/restart.test.js
+++ test/src/API/projects/restart.test.js
@@ -13,7 +13,7 @@ const path = require('path');
 const chaiResValidator = require('chai-openapi-response-validator');
 
 const projectService = require('../../../modules/project.service');
-const { 
+const {
     testTimeout,
     TEMP_TEST_DIR,
     pathToApiSpec,
@@ -26,8 +26,8 @@ describe('Project Restart Tests (POST /projects/{id}/restart)', function() {
     const projectName = `test-project-restart-${Date.now()}`;
     const pathToLocalProject = path.join(TEMP_TEST_DIR, projectName);
     let projectID;
-    
-    before('create a sample project and bind to Codewind, without building', async function() {
+
+    before('create a sample Node.js project and bind to Codewind, without building', async function() {
         this.timeout(testTimeout.med);
         projectID = await projectService.createProjectFromTemplate(projectName, 'nodejs', pathToLocalProject);
     });
@@ -36,45 +36,44 @@ describe('Project Restart Tests (POST /projects/{id}/restart)', function() {
         this.timeout(testTimeout.med);
         await projectService.removeProject(pathToLocalProject, projectID);
     });
-    
-    // Node can start in run, debug or debugNoInit modes.
+
+    // Node can start in 'run' or 'debugNoInit' modes, but not 'debug'.
     it('returns 400 when `restartMode` is `run` but project is still building', async function() {
         this.timeout(testTimeout.short);
         const res = await projectService.restartProject(projectID, 'run');
-        
+
         res.should.have.status(400);
         res.should.satisfyApiSpec;
         res.body.message.should.equal(`Request error for project ${projectID}. Restart is invalid when the project is building.`);
     });
-    
-    // does not currently work due to issue https://github.com/eclipse/codewind/issues/1968
-    it.skip('returns 400 when `restartMode` is `debug` but project is still building', async function() {
-        this.timeout(testTimeout.short);
-        const projectID = '022f9990-4375-11ea-9b24-c76506c783f7';
-        const res = await projectService.restartProject(projectID, 'debug');
-        
-        res.should.have.status(400);
-        res.should.satisfyApiSpec;
-        res.body.message.should.equal(`Request error for project ${projectID}. Restart is invalid when the project is building.`);
-    });
-    
+
     it('returns 400 when `restartMode` is `debugNoInit` but project is still building', async function() {
         this.timeout(testTimeout.short);
         const res = await projectService.restartProject(projectID, 'debugNoInit');
-        
+
         res.should.have.status(400);
         res.should.satisfyApiSpec;
         res.body.message.should.equal(`Request error for project ${projectID}. Restart is invalid when the project is building.`);
     });
-    
-    it.skip('returns 400 when `restartMode` is invalid', async function() {
+
+    it('returns 400 when `restartMode` is `debug`', async function() {
+        this.timeout(testTimeout.short);
+        const res = await projectService.restartProject(projectID, 'debug');
+
+        res.should.have.status(400);
+        res.should.satisfyApiSpec;
+        res.body.message.should.equal(`Request error for project ${projectID}. Node.js projects do not support restarting in debug mode.`);
+    });
+
+    it('returns 400 when `restartMode` is invalid', async function() {
         this.timeout(testTimeout.short);
         const res = await projectService.restartProject(projectID, 'unknownMode');
-        
+
         res.should.have.status(400);
+        res.should.satisfyApiSpec;
         res.text.should.equal('Error while validating request: request.body.startMode should be equal to one of the allowed values');
     });
-    
+
     it('returns 404 when project does not exist', async function() {
         this.timeout(testTimeout.short);
         const projectID = '00000000-0000-0000-0000-000000000000';
