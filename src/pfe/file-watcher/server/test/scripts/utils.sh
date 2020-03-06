ad4c0778f863c0b97017d88edfb381c23c6c77c6
ssh24 <sakib@ibm.com>
2020-03-06 15:24:20 -0500

    Have the ability to enable/disable ssl on functional tests for che >= 7.9.0
    Signed-off-by: ssh24 <sakib@ibm.com>

--- src/pfe/file-watcher/server/test/scripts/utils.sh
+++ src/pfe/file-watcher/server/test/scripts/utils.sh
@@ -22,10 +22,15 @@ export TEST_INFO_DIR="$HOME/.codewindtest/turbine"
 # Function to get the che and keycloak endpoint
 function getEndpoints() {
     echo -e "${BLUE}Getting che endpoints${RESET}\n"
+    if [ ! -z "$DISABLE_SSL" ]; then
+        PROTOCOL="http://"
+    else
+        PROTOCOL="https://"
+    fi
 
-    CHE_ENDPOINT=$(kubectl get routes --selector=component=che -o jsonpath="{.items[0].spec.host}" 2>&1)
-    KEYCLOAK_HOSTNAME=$(kubectl get routes --selector=component=keycloak -o jsonpath="{.items[0].spec.host}" 2>&1)
-    TOKEN_ENDPOINT="http://${KEYCLOAK_HOSTNAME}/auth/realms/che/protocol/openid-connect/token"
+    CHE_ENDPOINT="${PROTOCOL}$(kubectl get routes --selector=component=che -o jsonpath="{.items[0].spec.host}" 2>&1)"
+    KEYCLOAK_HOSTNAME="${PROTOCOL}$(kubectl get routes --selector=component=keycloak -o jsonpath="{.items[0].spec.host}" 2>&1)"
+    TOKEN_ENDPOINT="${KEYCLOAK_HOSTNAME}/auth/realms/che/protocol/openid-connect/token"
 }
 
 # Function to generate the Che Access Token for Che User Authentication
@@ -36,7 +41,7 @@ function generateCheAccessToken {
 
     getEndpoints
 
-    CHE_ACCESS_TOKEN=$(curl -sSL --data "grant_type=password&client_id=che-public&username=${CHE_USER}&password=${CHE_PASS}" ${TOKEN_ENDPOINT} | jq -r '.access_token')
+    CHE_ACCESS_TOKEN=$(curl -k -sSL --data "grant_type=password&client_id=che-public&username=${CHE_USER}&password=${CHE_PASS}" ${TOKEN_ENDPOINT} | jq -r '.access_token')
 }
 
 function checkExitCode() {
