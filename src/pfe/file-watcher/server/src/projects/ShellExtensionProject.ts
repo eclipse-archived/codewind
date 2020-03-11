0c0adaa2f108bfc241a8a9a4fac75e20880006bb
Andrew Mak <makandre@ca.ibm.com>
2020-03-11 16:17:14 -0400

    buildByExtension
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>

--- src/pfe/file-watcher/server/src/projects/ShellExtensionProject.ts
+++ src/pfe/file-watcher/server/src/projects/ShellExtensionProject.ts
@@ -87,6 +87,7 @@ export class ShellExtensionProject implements IExtensionProject {
 
     supportedType: string;
     detectChangeByExtension: boolean | string[] = true;
+    buildByExtension: boolean = true;
 
     private fullPath: string;
     private config: ShellExtensionProjectConfig;
