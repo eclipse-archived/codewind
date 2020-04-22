5e11a7a06a037a385366abef524bfc2f29dba7d3
markcor11 <mark.cornaia@uk.ibm.com>
2020-04-22 19:00:56 +0100

    Added mock API for developing
    Signed-off-by: markcor11 <mark.cornaia@uk.ibm.com>

--- src/pfe/portal/routes/projects/projects.route.js
+++ src/pfe/portal/routes/projects/projects.route.js
@@ -35,6 +35,31 @@ router.get('/api/v1/projects/:id', (req, res) => {
 });
 
 /**
+ * API Function to get project capabilities of a single project and return it as a json object
+ * @return the project capabilities object
+ */
+router.get('/api/v1/projects/capabilities/:id', (req, res) => {
+  try {
+    // Just a mocked up API response for testing perf dashboard
+    const projectCapabilities = {
+      running: true,
+      capabilities: {
+        loadrunner: true,
+        liveMetrics: false,
+        metricsCollections: true,
+        metricsCollectionsTimed: false,
+        profiling: false
+      }
+    }
+    res.status(200).send(projectCapabilities);
+  } catch (err) {
+    log.error(err);
+    res.status(500).send(err);
+  }
+});
+
+
+/**
  * API Function to get the projectList and return it as an array
  * @return the projectList
  */
